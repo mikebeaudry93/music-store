ae15c17b49022dd839874029a99242222150ac73
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-10-08 14:57:57 -0400

    initial build complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Element, scroller } from "react-scroll";
 
 import Hero from "../components/Hero";
 import ProductLinks from "../components/ProductLinks";
 
 const Home = () => {
+  const scrollToElement = (element) => {
+    scroller.scrollTo(element, {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: -100,
+    });
+  };
+
   return (
     <div>
       <Hero>
-        <Link className="btn btn-hero">products</Link>
+        <Link
+          className="btn btn-hero"
+          onClick={() => scrollToElement("product-links")}
+        >
+          products
+        </Link>
       </Hero>
-      <ProductLinks />
+      <Element name="product-links">
+        <ProductLinks />
+      </Element>
     </div>
   );
 };

ac0b7453656f7dab6631cca4ceb011bad2862256
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-10-16 16:08:39 -0400

    removed carousel

--- src/components/Products/FeaturedProducts.js
+++ src/components/Products/FeaturedProducts.js
@@ -1,47 +1,16 @@
 import React from "react";
 import { ProductContext } from "../../context/products";
-import Carousel from "react-multi-carousel";
+
 import "react-multi-carousel/lib/styles.css";
 
-import Product from "./Product";
+// import Product from "./Product";
+import ProductList from "./ProductList";
 
 const FeaturedProducts = () => {
-  const responsive = {
-    largeDesktop: {
-      breakpoint: { max: 3000, min: 1600 },
-      items: 4,
-      slidesToSlide: 1,
-    },
-    desktop: {
-      breakpoint: { max: 1438, min: 1024 },
-      items: 3,
-      slidesToSlide: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1,
-    },
-  };
   const { featured } = React.useContext(ProductContext);
   return (
     <div>
-      <h2 className="featured-title">featured products</h2>
-      <Carousel
-        responsive={responsive}
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={4000}
-      >
-        {featured.map((item) => {
-          return <Product key={item.id} {...item} />;
-        })}
-      </Carousel>
+      <ProductList title="Featured" products={featured} />;
     </div>
   );
 };
@@ -49,3 +18,4 @@ const FeaturedProducts = () => {
 export default FeaturedProducts;
 
 // <ProductList title="featured products" products={featured} />
+// <h2 className="featured-title">featured products</h2>

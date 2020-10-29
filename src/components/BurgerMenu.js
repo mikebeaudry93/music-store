00a9fbc34a500881d5393d817a87117d5fcc3cc6
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-10-29 19:50:01 -0400

    final

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,43 +1,54 @@
-import React from 'react'
-import {slide as Menu} from 'react-burger-menu'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { Link } from "react-router-dom";
 
 class BurgerMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
 
-    constructor (props) {
-        super(props)
-        this.state = {
-          menuOpen: false
-        }
-      }
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
 
-      handleStateChange (state) {
-        this.setState({menuOpen: state.isOpen})  
-      }
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
 
-      closeMenu () {
-        this.setState({menuOpen: false})
-      }
-    
-      render() {
-
+  render() {
     return (
-        <Menu
-        onStateChange={(state) => this.handleStateChange(state)} 
-        isOpen={this.state.menuOpen} 
+      <Menu
+        onStateChange={(state) => this.handleStateChange(state)}
+        isOpen={this.state.menuOpen}
         className="burger-menu"
-        >
-            <div className="burger-menu-links">
-                <Link onClick={() => this.closeMenu()} to="/">Home</Link>
-                <Link onClick={() => this.closeMenu()} to="/about">About</Link>
-                <Link onClick={() => this.closeMenu()} to="/guitars">Guitars</Link>
-                <Link onClick={() => this.closeMenu()} to="/bass">Bass</Link>
-                <Link onClick={() => this.closeMenu()} to="/drums">Drums</Link>
-                <Link onClick={() => this.closeMenu()} to="/amps">Amps</Link>
-            </div>
-        </Menu>
-        )
-    }
+        disableAutoFocus
+      >
+        <div className="burger-menu-links">
+          <Link onClick={() => this.closeMenu()} to="/">
+            Home
+          </Link>
+          <Link onClick={() => this.closeMenu()} to="/about">
+            About
+          </Link>
+          <Link onClick={() => this.closeMenu()} to="/guitars">
+            Guitars
+          </Link>
+          <Link onClick={() => this.closeMenu()} to="/bass">
+            Bass
+          </Link>
+          <Link onClick={() => this.closeMenu()} to="/drums">
+            Drums
+          </Link>
+          <Link onClick={() => this.closeMenu()} to="/amps">
+            Amps
+          </Link>
+        </div>
+      </Menu>
+    );
+  }
 }
 
-export default BurgerMenu
+export default BurgerMenu;
